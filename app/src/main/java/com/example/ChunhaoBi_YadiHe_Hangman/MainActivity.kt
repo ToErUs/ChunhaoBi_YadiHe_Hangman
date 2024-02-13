db9a2017fa4d1e6fe2682ed6c7236f342afaaed4
bch1755 <bch1755@outlook.com>
2024-02-13 16:58:53 -0500

    Modify word by adding space between characters

--- app/src/main/java/com/example/ChunhaoBi_YadiHe_Hangman/MainActivity.kt
+++ app/src/main/java/com/example/ChunhaoBi_YadiHe_Hangman/MainActivity.kt
@@ -10,13 +10,6 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.util.TypedValue
-import android.view.Gravity
-import android.widget.LinearLayout
-import androidx.core.graphics.drawable.toBitmap
-
 import androidx.lifecycle.ViewModelProvider
 import com.example.ChunhaoBi_YadiHe_Hangman.databinding.LayoutBinding
 
@@ -102,7 +95,6 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-
         var displayWord = binding.word.text.toString()
         for(i in 0 until LETTER_NUM) {
             if (usedLetters[i]) {
@@ -169,6 +161,7 @@ class MainActivity : ComponentActivity() {
                 for (i in 0 until  LETTER_NUM) {
                     if (!usedLetters[i] && 'A'+ i !in targetWord) {
                         disable = if (disable) {
+                            usedLetters[i] = true
                             disableButtonByLetter('A'+ i)
                             false
                         } else
@@ -237,44 +230,26 @@ class MainActivity : ComponentActivity() {
         binding.hintMsg.text = "Hint: "+hintString
     }
     private fun setGuessedWord(word:String){
-        val density = resources.displayMetrics.density
-
         //_word should be a combination of spaces and guessed letters, eg. "  AM l "
         binding.word.text = word
         Log.d(TAG,	"Set word to "+word)
 
-        val linearLayout = findViewById<LinearLayout>(R.id.wordBox)
-
-        linearLayout.removeAllViews();
+        binding.wordBox.removeAllViews();
         for (char in word) {
-            val textView = TextView(this)
-            val textSizeInSp = 16 // Set your desired text size in sp
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeInSp.toFloat())
-            val scaledPixelsToPixels = textSizeInSp * density
-            val widthInPixels = scaledPixelsToPixels.toInt() * 3 // Adjust the multiplier as needed
-            val heightInPixels = scaledPixelsToPixels.toInt() * 3
-            val margin=(scaledPixelsToPixels*0.5).toInt()
-            val padding = (scaledPixelsToPixels*0.5).toInt()
-            textView.setText(char.toString())
-            textView.width = widthInPixels
-            textView.height = heightInPixels
-            //textView.setBackgroundColor(Color.GRAY)
-            textView.gravity = Gravity.CENTER
-            val drawable = resources.getDrawable(R.drawable.underline, null) // Replace `your_image` with the name of your image file
-            val bitmapDrawable = BitmapDrawable(resources, drawable.toBitmap(widthInPixels, heightInPixels, Bitmap.Config.ARGB_8888))
-            textView.background = bitmapDrawable
-
-            // Set padding to ensure the text is centered both horizontally and vertically
-             // Adjust padding as needed
-            textView.setPadding(padding, padding, padding, padding)
-            val layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            layoutParams.setMargins(margin, margin, margin, margin)
-            textView.layoutParams = layoutParams
-
-            linearLayout.addView(textView)
+            val textView = TextView(this,null, 0, R.style.textViewRow)
+//            val textView = TextView(this)
+//            setTextAppearance(textView, R.style.textViewRow)
+            if (char == ' ') {
+                textView.setText("  ")
+            }
+            else {
+                textView.setText(char.toString())
+            }
+            textView.paintFlags = textView.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+            binding.wordBox.addView(textView)
+            val emptyView = TextView(this,null, 0, R.style.textViewRow)
+            emptyView.setText("  ")
+            binding.wordBox.addView(emptyView)
         }
     }
     private fun enableAllButtons(){
